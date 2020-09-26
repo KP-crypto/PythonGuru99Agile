14fb60c00f74a0ee917a82ba665b435f0a846b74
KP-crypto <puranik324@gmail.com>
2020-09-26 13:58:55 +0530

    commit

--- TestCases/conftest.py
+++ TestCases/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
@@ -8,7 +9,9 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 @pytest.fixture()
 def setup(browser):
     if browser=="chrome":
-        driver=webdriver.Chrome(ChromeDriverManager().install())
+        chrome_options=Options()
+        chrome_options.add_argument("--headless")
+        driver=webdriver.Chrome(ChromeDriverManager().install(),options=chrome_options)
     elif browser=="firefox":
         driver=webdriver.Firefox(executable_path=GeckoDriverManager().install())
     else:
