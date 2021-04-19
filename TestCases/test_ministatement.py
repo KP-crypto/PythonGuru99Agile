23d5aef7ca0e65b90465eea79cb499132797f2c8
KP-crypto <puranik324@gmail.com>
2021-04-19 19:23:44 +0530

    latest commit 4

--- TestCases/test_ministatement.py
+++ TestCases/test_ministatement.py
@@ -1,5 +1,4 @@
 import time
-from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from Pages.CustomerPage import CustomerPage
 from Pages.Homepage import HomePage
@@ -7,11 +6,11 @@ from Utilities.readrequirement import ReadConfig
 import pytest
 
 class Test_MiniStatement():
+
     url = ReadConfig.getApplicationURL()
     userid = ReadConfig.getUserid()
     password = ReadConfig.getPassword()
 
-    @pytest.mark.run(order=2)
     def test_miniStatement(self,setup):
         self.driver = setup
         self.driver.get(self.url)
@@ -33,14 +32,15 @@ class Test_MiniStatement():
             rows=self.cp.verify_row_count()
             print(len(rows))
 
-            if len(rows)=='5':
+            if len(rows) > 0:
                 assert True
-                self.driver.close()
+
             else:
                 assert False
         except Exception as e:
             print(e)
-            self.driver.close()
+        self.driver.close()
+
 
 
 
